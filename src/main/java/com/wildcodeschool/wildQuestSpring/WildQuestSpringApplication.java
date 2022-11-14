e5c7a490bee0cc7cbef71909f68452e9723db01b
Uriel SAUTRON <uriel.sautron@gmail.com>
2022-11-14 10:48:51 +0100

    Init and first commit

---
+++ src/main/java/com/wildcodeschool/wildQuestSpring/WildQuestSpringApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.wildQuestSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildQuestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildQuestSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/wildQuestSpring/WildQuestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.wildQuestSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildQuestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
